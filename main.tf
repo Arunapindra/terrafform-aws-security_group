5c95f2a6539009a060702ca3a218b023074ef93f
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-18 00:40:28 -0500

    creating vpc and mysql security group from 2 modules [terraform-aws-vpc][terraform-aws-security-group]

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "main" {
+  name        = local.final_sg_name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+  tags = merge(var.common_tags,var.sg_tags,
+    {
+    Name = local.final_sg_name
+  })
+}
+
+
+resource "aws_security_group" "sg_egress" {
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" { 
+
+}
+variable "environment" {
+  
+}
+variable "component" {
+  
+}
+variable "vpc_id" {
+  
+}
+variable "common_tags" {
+  default = {}
+}
+variable "sg_tags" {
+  default = {}
+}
+variable "tags" {
+  default = {}
+}
